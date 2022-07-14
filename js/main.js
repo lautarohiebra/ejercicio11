382cd844e9e8f3fe7fd29df13b6a76e92e646a0c
Lautaro Hiebra <lautarohiebra@gmail.com>
2022-07-14 02:50:10 -0300

    Version 1 finalizada con boton de cambio de Estilos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 console.log(document.querySelector('title').innerText);
+
+let estilo1="../css/estilos-futuro.css"
+let estilo2="../css/estilos-retro.css"
+let estilo3="../css/estilos.css"
+
+const estilos=[estilo1,estilo2,estilo3]
+
+console.log(estilos);
+
+
+let i = "estilo-1"
+console.log("Actualmente visualizando el estilo Original");
+
+function cambio() {
+    if (i=="estilo-1") {
+        console.warn("Cambiando al segundo estilo (FUTURO)");
+        i="estilo-2";
+        console.log(i);
+        document.getElementById('css').href=("css/estilos-futuro.css")
+    }
+    else if (i=="estilo-2"){
+        console.warn("Cambiando al tercer estilo (RETRO)")
+        i="estilo-3"
+        console.log(i);
+        document.getElementById('css').href=("css/estilos-retro.css")
+    }
+    else if (i="estilo-3"){
+        console.warn("Volviendo al diseño original")
+        i="estilo-1"
+        console.log(i);
+        document.getElementById('css').href=("css/estilos.css")
+    }}
